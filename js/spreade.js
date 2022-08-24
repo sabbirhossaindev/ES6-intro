188de11120132c4968d1de36e50fa3f5c8f18e9d
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-24 07:28:22 +0600

    arrow function more explore

--- js/spreade.js
+++ js/spreade.js
@@ -3,7 +3,7 @@ const max = Math.max(12, 85, 999, 78);
 const numbers = [12, 85, 999, 78];
 const largest = Math.max(...numbers);
 // console.log(...numbers);
-// console.log(largest);
+console.log(largest);
 
 const numbers2 = [...numbers];
 numbers.push(55);
@@ -12,5 +12,7 @@ console.log(numbers);
 console.log(numbers2);
 
 const numbers3 = [...numbers];
+console.log(numbers3)
 
 const numbers4 = [444, 78, ...numbers, 111, 33]
+console.log(numbers4);
